67e5e978e576061f1f59a97d73bc132ddf4ce792
josepagan <josepaganrnj@outlook.com>
2026-04-13 11:39:53 -0400

    Step #4 completed added Menu and printed it

--- src/main/java/com/pluralsight/Menu.java
+++ src/main/java/com/pluralsight/Menu.java
@@ -2,9 +2,13 @@ package com.pluralsight;
 
 public class Menu {
     public static void main(String[] args) {
-
+    displayMenu();
     }
     public static void displayMenu(){
-        System.out.println("==");
+        System.out.println("=== Menu ===");
+        System.out.println("1. Coffee - $3.99");
+        System.out.println("2. Tea - $2.99");
+        System.out.println("3. Cookie - $1.99");
     }
 }
+
