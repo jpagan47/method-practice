35daa94910dbe47e8c738f17b0ad3795059ffd04
josepagan <josepaganrnj@outlook.com>
2026-04-13 13:03:29 -0400

    Added code comparing numbers, struggling a little bit on even or odd part

---
+++ src/main/java/com/pluralsight/CompareNumbers.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class CompareNumbers {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isEven(int number){
+        if (number %2) {
+
+    }
+}
